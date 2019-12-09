19c413f21d8e9eb0f8c34eabdf5cf88e0110ca32
BLAREL <alex627@orange.fr>
2019-12-09 13:59:40 +0100

    intersectioncluster

--- cluster.c
+++ cluster.c
@@ -1,4 +1,3 @@
-#pragma once
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
@@ -10,12 +9,20 @@
 
 
 
-LinkedList* clusterHeureAriver(LinkedList* listeclient, ElementListe test) {
+
+
+
+  
+
+
+
+LinkedList* clusterHeureAriver(LinkedList* list, ElementListe test) {
 	Time intevallecluster[2];
 	LinkedList* listCluster = NewLinkedList();
-	SingleLinkedListElem* CurrentElement = listeclient->head;
+
 	ElementListe elementAdd;
 	elementAdd = test;
+
 
 
 	InsertElementAt(listCluster, 0, elementAdd);
@@ -23,28 +30,28 @@ LinkedList* clusterHeureAriver(LinkedList* listeclient, ElementListe test) {
 	int g = 0;
 	int tampon = 0;
 	intevallecluster[0] = test.order.intervallearrivee[0];
-	intevallecluster[0].heure = intevallecluster[0].heure-1;
+	intevallecluster[0].heure = intevallecluster[0].heure - 1;
 	intevallecluster[1] = test.order.intervallearrivee[1];
 	intevallecluster[1].heure = intevallecluster[1].heure + 1;
-	
-	while (CurrentElement != listeclient->tail)
-	{
-		if ((est_sup(CurrentElement->info.order.intervallearrivee[0], intevallecluster[0]) == true) || (est_inf(CurrentElement->info.order.intervallearrivee[1], intevallecluster[1]) == true)) {
-			if (CurrentElement->info.order.ID != test.order.ID) {
-				elementAdd = CurrentElement->info;
+
+	SingleLinkedListElem* tmp;
+	for (int i = 0; i < list->size; i++) {
+		tmp = GetElementAt(list, i);
+		if ((est_sup(tmp->info.order.intervallearrivee[0], intevallecluster[0]) == true) || (est_inf(tmp->info.order.intervallearrivee[1], intevallecluster[1]) == true)) {
+			if (tmp->info.order.ID != test.order.ID) {
+				elementAdd = tmp->info;
 				InsertElementAt(listCluster, 1, elementAdd);
 			}
 		}
-		CurrentElement = CurrentElement->next;
 	}
-
+	printf("voici le cluster heure arrivee \n");
+	afficherListe(listCluster);
 	return(listCluster);
 }
 
-LinkedList* clusterHeureDepart(LinkedList* listeclient, ElementListe test) {
+LinkedList* clusterHeureDepart(LinkedList* list, ElementListe test) {
 	Time intevallecluster[2];
 	LinkedList* listCluster = NewLinkedList();
-	SingleLinkedListElem* CurrentElement = listeclient->head;
 
 	ElementListe elementAdd;
 	elementAdd = test;
@@ -58,60 +65,64 @@ LinkedList* clusterHeureDepart(LinkedList* listeclient, ElementListe test) {
 	intevallecluster[1] = test.order.intevalledepart[1];
 	intevallecluster[1].heure = intevallecluster[1].heure + 1;
 
-	while (CurrentElement != listeclient->tail)
-	{
-		if ((est_sup(CurrentElement->info.order.intevalledepart[0], intevallecluster[0]) == true) || (est_inf(CurrentElement->info.order.intevalledepart[1], intevallecluster[1]) == true)) {
-			if (CurrentElement->info.order.ID != test.order.ID) {
-				elementAdd = CurrentElement->info;
+	SingleLinkedListElem* tmp;
+	for (int i = 0; i < list->size; i++) {
+		tmp = GetElementAt(list, i);
+		if ((est_sup(tmp->info.order.intevalledepart[0], intevallecluster[0]) == true) || (est_inf(tmp->info.order.intevalledepart[1], intevallecluster[1]) == true)) {
+			if (tmp->info.order.ID != test.order.ID) {
+				elementAdd = tmp->info;
 				InsertElementAt(listCluster, 1, elementAdd);
 			}
 		}
-		CurrentElement = CurrentElement->next;
 	}
+	printf("voici le cluster heure depart \n");
+	afficherListe(listCluster);
 	return(listCluster);
 }
 
-LinkedList* clusterCapaciter(LinkedList* listeclient, ElementListe test, int capaActuel, int capMax) {
+LinkedList* clusterCapaciter(LinkedList* list, ElementListe test, int capaActuel, int capMax) {
 	LinkedList* listCluster = NewLinkedList();
 	ElementListe elementAdd;
-	SingleLinkedListElem* CurrentElement = listeclient->head;
 	elementAdd = test;
 	InsertElementAt(listCluster, 0, elementAdd);
 	int capaTest = capaActuel + elementAdd.order.nbrpersonne;
-	int cap=0;
-	while (CurrentElement != listeclient->tail)
-	{
+	int cap = 0;
+	SingleLinkedListElem* tmp;
+
+	for (int i = 0; i < list->size; i++) {
 		int cap = 0;
-		cap = capaTest + CurrentElement->info.order.nbrpersonne;
+		tmp = GetElementAt(list, i);
+		cap = capaTest + tmp->info.order.nbrpersonne;
+
 		if (cap < capMax) {
-			if (CurrentElement->info.order.ID != test.order.ID) {
-				elementAdd = CurrentElement->info;
+			if (tmp->info.order.ID != test.order.ID) {
+				elementAdd.order = tmp->info.order;
 				InsertElementAt(listCluster, 1, elementAdd);
 			}
 		}
-		CurrentElement = CurrentElement->next;
 	}
+	printf("voici le cluster capaciter de l element %d pour une capaactuelle %d pour une capamax %d \n ", test.order.ID, capaActuel, capMax);
+	afficherListe(listCluster);
 	return(listCluster);
 }
 
-LinkedList* clusterGeographique(LinkedList* listeclient, ElementListe client, int nbdemande, float dist) {
-	LinkedList* listCluster = NewLinkedList();
-	ElementListe elementAdd;
-	SingleLinkedListElem* CurrentElement = listeclient->head;
+LinkedList* clusterGeographique(LinkedList* list, ElementListe client, float dist) {
+
 	LinkedList* cluster = NewLinkedList();
 	InsertElementAt(cluster, 0, client);
 
-	while (CurrentElement != listeclient->tail)
-	{
-		if (distance(client.order.depart, CurrentElement->info.order.depart) < dist) {
-			if (CurrentElement->info.order.ID != client.order.ID) {
-				InsertElementAt(cluster, 1, CurrentElement->info);
+	 SingleLinkedListElem* tmp;
+	
+	for (int k = 0; k < list->size; k++) {
+		tmp = GetElementAt(list, k);
+		if (distance(client.order.depart, tmp->info.order.depart) < dist) {
+			if (tmp->info.order.ID != client.order.ID) {
+				InsertElementAt(cluster, k+1, tmp->info);
 			}
 		}
-		CurrentElement = CurrentElement->next;
 	}
-
 	printf("tous les clients proches a moins de %.2f metres du client %d sont :\n", dist, client.order.ID);
+	afficherListe(cluster);
 	return(cluster);
 }
 
@@ -124,6 +135,70 @@ float distance(Pos p1, Pos p2) {
 }
 
 int aleatoire(int taille) {
-		int valeur = (rand() * (taille - 0) / RAND_MAX);
-		 return valeur;
+
+	int valeur = (rand() * (taille - 0) / RAND_MAX);
+	return valeur;
+}
+
+int nbre_presence_cluster(SingleLinkedListElem* tmp, LinkedList* c1, LinkedList* c2, LinkedList* c3, LinkedList* c4) {
+	// je compte le nombre de clusters contenant tmp
+	int compteur = 0;
+	SingleLinkedListElem* a;
+	for (int k = 0; k < c1->size; k++) {
+		a = GetElementAt(c1, k);
+		if (a->info.order.ID == tmp->info.order.ID && tmp->info.isDeparture == true) {
+			compteur++;
+		}
+	}
+	for (int k = 0; k < c2->size; k++) {
+		a = GetElementAt(c2, k);
+		if (a->info.order.ID == tmp->info.order.ID && tmp->info.isDeparture== true) {
+			compteur++;
+		}
+	}
+	for (int k = 0; k < c3->size; k++) {
+		a = GetElementAt(c3, k);
+		if (a->info.order.ID == tmp->info.order.ID && tmp->info.isDeparture == true) {
+			compteur++;
+		}
+	}
+	for (int k = 0; k < c4->size; k++) {
+		a = GetElementAt(c4, k);
+		if (a->info.order.ID == tmp->info.order.ID && tmp->info.isDeparture == true) {
+			compteur++;
+		}
+	}
+	
+	return(compteur);
+}
+
+LinkedList* IntersecCluster(LinkedList* list) {
+
+	// je renvoie une linkedlist contenant les clients communs aux 4 clusters
+
+	int compteur = 0;
+	SingleLinkedListElem* tmp;
+	tmp = GetElementAt(list, 2);
+	
+	LinkedList* tampon = NewLinkedList();
+	LinkedList* tampon1 = NewLinkedList();
+	LinkedList* tampon2 = NewLinkedList();
+	LinkedList* tampon3 = NewLinkedList();
+	LinkedList* Intersec = NewLinkedList();
+
+	tampon = clusterCapaciter(list, tmp->info,0,3);
+	tampon1 = clusterGeographique(list, tmp->info, 5);
+	tampon2 = clusterHeureAriver(list, tmp->info);
+	tampon3 = clusterHeureDepart(list, tmp->info);
+	int emplacement = 0;
+	for (int k = 0; k < list->size; k++) {
+		tmp = GetElementAt(list, k);
+
+		if ( nbre_presence_cluster(tmp,tampon,tampon1,tampon2,tampon3)== 4 ){
+			InsertElementAt(Intersec, emplacement,tmp->info);
+			emplacement++;
+		}
+	}
+	printf("voici la liste de tous les clients contenus dans les 4 clusters \n");
+	return(Intersec);
 }
--- cluster.h
+++ cluster.h
@@ -8,19 +8,24 @@
 #include "Structures.h"
 
 //cluster temp arriver
-LinkedList* clusterHeureAriver(LinkedList* listeclient, ElementListe test);
+LinkedList* clusterHeureAriver(LinkedList* list, ElementListe test);
 
 //cluster temp depart
-LinkedList* clusterHeureDepart(LinkedList* listeclient, ElementListe test);
+LinkedList* clusterHeureDepart(LinkedList* list, ElementListe test);
 
 //cluster capaciter
-LinkedList* clusterCapaciter(LinkedList* listeclient, ElementListe test, int capaActuel, int capMax);
+LinkedList* clusterCapaciter(LinkedList* list, ElementListe test, int capaActuel, int capMax);
 
 //cluster de distance
-LinkedList* clusterGeographique(LinkedList* listeclient, ElementListe client, int nbdemande, float dist);
+LinkedList* clusterGeographique(LinkedList* list, ElementListe client, float dist);
 
 //calcule distance 
 float distance(Pos p1, Pos p2);
 
 //aleatoire
 int aleatoire(int taille);
+
+
+LinkedList* IntersecCluster(LinkedList* list);
+
+int nbre_presence_cluster(SingleLinkedListElem* tmp, LinkedList* c1, LinkedList* c2, LinkedList* c3, LinkedList* c4);
