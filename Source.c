196d261dff78f2fd0bc1fc915ec3f340c3256b06
BLAREL <alex627@orange.fr>
2019-12-19 15:15:37 +0100

    graphviz

--- Source.c
+++ Source.c
@@ -5,158 +5,159 @@
 #include <math.h>
 #include "liste.h"
 #include "check.h"
+#include "cluster.h"
+#include "Structures.h"
+
+
 #define TAILLE_MAX 1000 // Tableau de taille 1000
 
 
 
-// on compare toutes les distances, on ne retient que celles inférieures à 5
+FILE RouteDrawing(char NomDuFichierOuNomDuChemin[], LinkedList* MaRoute) {
 
+	//Ouverture du fichier texte en veillant à pouvoir écrire dedans
+	FILE* fichier = fopen(NomDuFichierOuNomDuChemin, "r+");
 
-float distance(Pos p1, Pos p2) {
-
-	return(sqrt(pow((p1.X - p2.X), 2) + pow((p1.Y - p2.Y), 2)));
-
-}
-
-LinkedList* clustergeographique(ElementListe list[100] , ElementListe client, int nbdemande, int dist) {
-
-	LinkedList* cluster = NewLinkedList();
-	
-
-	for (int k = 0; k < nbdemande + 1; k++) {
-
-		if (distance(client.order.depart, list[k].order.depart) < dist) {
-			InsertElementAt(cluster, 0, list[k]);
-		}
+	//Verifions que le pointeur a bien pris le fichier
+	if (fichier != NULL)
+	{
+		//OK
 	}
-	printf("tous les clients proches a moins de %d metres du client %d sont :\n", dist, client.order.ID);
-	return(cluster);
+	else
+	{
+		//On affiche un message d'erreur si on veut
+		printf("Impossible d'ouvrir le fichier");
+	}
+
+	//Nous regardons les éléments de la liste chainées et commençons à dessiner notre schéma graphviz
+	SingleLinkedListElem* current;
+	SingleLinkedListElem* currentsuivant;
+	current = GetElementAt(MaRoute, 0);
+	currentsuivant = GetElementAt(MaRoute, 1);
+	fprintf(fichier, "digraph G{");
+	while (current != MaRoute->tail) {
+
+		fprintf(fichier, "%d->%d ;\n",current->info.order.ID,currentsuivant->info.order.ID);
+
+		current = currentsuivant;
+		currentsuivant = currentsuivant->next;
+	}
+
+	fprintf(fichier, "}");
+
+
 }
-	
 
 
 int main() {
+	LinkedList* liste;
+	liste = lecture_fichier("a4-40.txt");
+	liste = Initialisation(liste);
+	afficherListe(liste);
 
-	FILE* fichier = NULL;
+	LinkedList* copy;
+	copy = CopyList(liste);
 
-	fichier = fopen("a4-40.txt", "r");
-	int nbdetaxi, nbdemande, dureetravail, capacite, dureemaxdemande;
-	printf("coucou \n");
-	int index;
-	float abscisses;
-	float ordonnees;
-	int capacité;
-	int nbclient;
-	int tempsdepart;
-	int tempsarrivee;
-	ElementListe listeclient[100];
+	
+	printf("is departure %d \n", copy->tail->info.isDeparture);
 
 
 
-	//on ouvre le fichier texte pour prélever tous les documents
-	if (fichier != NULL)
-	{
-		int lire;
-		lire = fscanf(fichier, "%d %d %d %d %d", &nbdetaxi, &nbdemande, &dureetravail, &capacite, &dureemaxdemande);
-		printf("je suis dans le fichier\n");
-		printf("%d\n", nbdetaxi);
-
-
-
-
-
-		for (int k = 0; k < nbdemande + 1; k++) { 
-
+	float somme = 0;
+	Pos x1;
+	Pos x2;
+	SingleLinkedListElem* a;
+	for (int k=0; k< copy->size; k++){
+		a = GetElementAt(copy, k);
+		if (a->info.isDeparture == true) {
+			x1 = a->info.order.depart;
+			k++;
+		}
+		else {
+			x1 = a->info.order.arrivee;
+			k++;
+		}
+		if (a->next->info.isDeparture == true) {
+			x2 = a->info.order.depart;
 			
-			int lecture1 = fscanf(fichier, "%d %f %f %d %d %d %d", &index, &abscisses, &ordonnees, &capacite, &nbclient, &tempsdepart,
-				&tempsarrivee);
-
-
-
-			listeclient[k].order.ID = index;
-			printf("%d     ", listeclient[k].order.ID);
-			listeclient[k].order.depart.X = abscisses;
-			printf("%f   ", listeclient[k].order.depart.X);
-			listeclient[k].order.depart.Y = ordonnees;
-			printf("%f   ", listeclient[k].order.depart.Y);
-			listeclient[k].order.nbrpersonne = nbclient;
-			printf("%d   ", listeclient[k].order.nbrpersonne);
-			listeclient[k].order.intevalledepart[0].heure = tempsdepart / 60;
-			printf("%d:", listeclient[k].order.intevalledepart[0].heure);
-			listeclient[k].order.intevalledepart[0].minute = tempsdepart % 60;
-			printf("%d   ", listeclient[k].order.intevalledepart[0].minute);
-			listeclient[k].order.intevalledepart[1].heure = tempsarrivee / 60;
-			printf("%d:", listeclient[k].order.intevalledepart[1].heure);
-			listeclient[k].order.intevalledepart[1].minute = tempsarrivee % 60;
-			printf("%d\n", listeclient[k].order.intevalledepart[0].minute);
 		}
-
-		for (int k = 1; k <  nbdemande + 1; k++) { 
-
-			int lecture2 = fscanf(fichier, "%d %f %f %d %d %d %d", &index, &abscisses, &ordonnees, &capacite, &nbclient, &tempsdepart,
-				&tempsarrivee);
-
-			printf("%d     ", index);
-			listeclient[k].order.arrivee.X = abscisses;
-			printf("%f   ", listeclient[k].order.arrivee.X);
-			listeclient[k].order.arrivee.Y = ordonnees;
-			printf("%f   ", listeclient[k].order.arrivee.Y);
-			listeclient[k].order.nbrpersonne = nbclient;
-			printf("%d   ", listeclient[k].order.nbrpersonne);
-			listeclient[k].order.intervallearrivee[0].heure = tempsdepart / 60;
-			printf("%d:", listeclient[k].order.intervallearrivee[0].heure);
-			listeclient[k].order.intervallearrivee[0].minute = tempsdepart % 60;
-			printf("%d   ", listeclient[k].order.intervallearrivee[0].minute);
-			listeclient[k].order.intervallearrivee[1].heure = tempsarrivee / 60;
-			printf("%d:", listeclient[k].order.intervallearrivee[1].heure);
-			listeclient[k].order.intervallearrivee[1].minute = tempsarrivee % 60;
-			printf("%d\n", listeclient[k].order.intervallearrivee[0].minute);
-
+		else {
+			x2 = a->next->info.order.arrivee;
+		
 		}
-
-
-		fclose(fichier); // On ferme le fichier qui a été ouvert
-
-
+		somme += distance(x1, x2);
 	}
+	
+	printf(" voici la distance totale :  %f \n",somme);
+
+	printf("\n");
+	printf("j affiche ici ma liste copy \n");
+	afficherListe(copy);
+	printf("\n");
+
+	RouteDrawing("demo.dot",copy );
+
+
 
 	
 
 
+	//LinkedList* route;
 
+	//route = InsertDepot(copy);
+	//DeletCopy(copy, 0);
+
+
+	//afficherListe(route);
+	//printf("\n");
+
+	//int tampon;
+	//for (int i = 0; i < 2; i++)
+	//{
+	//	tampon = aleatoire(40, 0);
+	//}
+	//route = InsertElementAleatoire(tampon, copy, route);
+	//route = InitFirstElement(route);
+	//afficherListe(route);
+	//printf("\n");
+	//afficherListeHeure(route);
+	//printf("\n affichage de copy : ");
+
+	//afficherListe(copy);
+	//printf("\n");
+
+	//LinkedList* rout;
+	//SingleLinkedListElem* tmpo;
+	//tmpo = GetElementAt(copy, 1);
+	//rout = IntersecClusters(copy, tmpo, 1);
+	//printf("\n interssection cluster: ");
+
+	//afficherListe(rout);
+
+
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	tampon = aleatoire(route->size, 1);
+	//}
+	//rout = IntersecClusters(copy, tmpo, 1);
+	//int i = 0;
+	//i = InsertGoodSpot(copy, route, rout, tampon);
+	//afficherListe(route);
+	//afficherListe(copy);
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	tampon = aleatoire(route->size, 1);
+	//}
+	//rout = IntersecClusters(copy, tmpo, 1);
+	//i = InsertGoodSpot(copy, route, rout, tampon);
+	//printf("\n afficherListe ");
+	//afficherListe(route);
+	//afficherListeIstaken(route);
+	//printf("\n affichercopy ");
 	
-	//on parcourt toutes les lignes pour connaitre chaque cluster
-	// on doit inclure le code du cluster ici:
 
 
-
-
-	LinkedList* tmp;
-	tmp=clustergeographique(listeclient, listeclient[1], nbdemande,5);
-
-	afficherListe(tmp);
-
-
-
-
-
-
-
-
-
-
-
-
-
-	system("pause");
-	return (0);
-
-
-
+	system("dot -Tpng demo.dot -o demo_dot.png");
+	return (EXIT_SUCCESS);
 }
-
-
-
-
-
 
